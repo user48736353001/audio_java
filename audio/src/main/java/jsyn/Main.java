9a27cc8b76433ff45ae5b524930949c504bf8d95
Andrés <acanabalponcela@gmail.com>
2022-06-19 21:01:20 +0200

    git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/4ndrea5/jsynth.git git push -u origin main

--- audio/src/main/java/jsyn/Main.java
+++ audio/src/main/java/jsyn/Main.java
@@ -2,6 +2,9 @@ package jsyn;
 
 import com.jsyn.*; //añadimos librería jsyn
 import com.jsyn.devices.AudioDeviceManager;
+import com.jsyn.unitgen.LineOut;
+import com.jsyn.unitgen.SpectralFilter;
+import com.jsyn.unitgen.WhiteNoise;
 
 
 public class Main {
@@ -10,10 +13,36 @@ public class Main {
         int numInputChannels=2;
         int numOutputChannels=2;
 
+        LineOut myOut;
+        WhiteNoise myNoise;
+        SpectralFilter myFilter;
+
 
        Synthesizer synth = JSyn.createSynthesizer();
        synth.start(i, AudioDeviceManager.USE_DEFAULT_DEVICE, numInputChannels, AudioDeviceManager.USE_DEFAULT_DEVICE,
                numOutputChannels);
+
+       //creación de generadores de onda
+
+        synth.add( myOut = new LineOut());
+        //generador de ruido blanco
+        synth.add( myNoise = new WhiteNoise());
+       //creación de filtro;
+        synth.add( myFilter = new SpectralFilter());
+
+        //conexionado de los generadores.
+        myNoise.output.connect(myFilter.input);
+
+
+
+
+
+
+        //salidas estéreo
+        myFilter.output.connect(0,myOut.input,0);
+        myFilter.output.connect(0,myOut.input,1);
+
+
        synth.stop();//finalización de ejecucion de Jsynth
     }
 }
